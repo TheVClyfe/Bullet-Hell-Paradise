003a9d80920386f37e161f95e1bee992957a4499
derozio <paradoxcrusher@gmail.com>
2020-05-02 17:59:27 +0530

    BHP-11: Adds BG Music, with Start and GameOver Scenes

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 2f;
+
+   public void LoadStartMenu() {
+        SceneManager.LoadScene(0);
+   }
+
+   public void LoadGameScene() {
+        SceneManager.LoadScene(1);    
+   }
+
+   public void LoadGameOverScene() {
+        StartCoroutine(WaitAndLoad());
+   }
+
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene(2);
+    }
+
+   public void QuitGame() {
+        Application.Quit(); 
+   }
+}
